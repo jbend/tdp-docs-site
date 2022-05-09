6bebccc67a0651eb9629d0e49c798da8070c42ca
Jeff Bendixsen <jeff_bendixsen@trimble.com>
2022-05-09 15:14:45 -0700

    Clean up

--- lib/azure.ts
+++ lib/azure.ts
@@ -1,13 +1,8 @@
 import { TableClient } from "@azure/data-tables";
-// const env = require("../configure/env");
-
-// const NEXT_PUBLIC_AZURE_TABLES_CONNECTION_STRING = 'DefaultEndpointsProtocol=https;AccountName=tdp-docs-db;AccountKey=qU8OhcxZ3yW2vBx1hl4p3NylE8LWohnZCuuxI1nKc19z8PgCKNTkYEy9C5RFUeiw68mf6PPTM2MBaemGDL9lLg==;TableEndpoint=https://tdp-docs-db.table.cosmos.azure.com:443/;';
-// const NEXT_PUBLIC_TABLE_MARKDOWN_LINKS = 'MarkdownLinks';
-
 const serviceClient = null; //TableClient.fromConnectionString(
-//   process.env.AZURE_TABLES_CONNECTION_STRING,
-//   process.env.TABLE_MARKDOWN_LINKS
-// );
+  process.env.AZURE_TABLES_CONNECTION_STRING,
+  process.env.TABLE_MARKDOWN_LINKS
+);
 
 export interface MarkdownLink {
   etag: string;
